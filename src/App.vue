3d11bf2eaae6c77f84a86617ce34c210d1c1271a
Dragon0617 <103638567+Dragon617@users.noreply.github.com>
2024-05-04 20:21:04 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,12 @@
   <el-card>
     <el-row type="flex" justify="center">
       <el-col :span="2">
+          //el-menu-item index="/donate">
+          //打赏/改需求
+          //el-menu-item>
+          //el-menu-item index="/group">
+          //交流/进群聊
+          //el-menu-item>
         <el-menu 
           router
           background-color="#545c64" 
@@ -9,12 +15,6 @@
           :default-active="$route.path"
           style="height: 750px;"
         >
-          //el-menu-item index="/donate">
-          //打赏/改需求
-          //el-menu-item>
-          //el-menu-item index="/group">
-          //交流/进群聊
-          //el-menu-item>
           <el-menu-item index="/">
             首页/生成器
           </el-menu-item>
